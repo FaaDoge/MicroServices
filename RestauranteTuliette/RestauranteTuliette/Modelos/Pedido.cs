b461e6945fdcfb69979a92ae55a65f7f03949bc2
Fabricio Sanchez <132334527+FaaDoge@users.noreply.github.com>
2024-01-15 23:17:01 -0400

    Revert "fabricio sanchez"

---
+++ RestauranteTuliette/RestauranteTuliette/Modelos/Pedido.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestauranteTuliette.Modelos;
+
+[Table("Pedido")]
+public partial class Pedido
+{
+    [Key]
+    public int IdPedido { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? NombreCliente { get; set; }
+
+    [StringLength(20)]
+    [Unicode(false)]
+    public string? TipodePago { get; set; }
+
+    [Column(TypeName = "decimal(18, 0)")]
+    public decimal? PrecioTotal { get; set; }
+
+    [Column("idUbicacion")]
+    public int? IdUbicacion { get; set; }
+
+    public int? IdPlato { get; set; }
+
+    public int? IdBebida { get; set; }
+
+    [ForeignKey("IdBebida")]
+    [InverseProperty("Pedidos")]
+    public virtual Bebidum? IdBebidaNavigation { get; set; }
+
+    [ForeignKey("IdPlato")]
+    [InverseProperty("Pedidos")]
+    public virtual Plato? IdPlatoNavigation { get; set; }
+
+    [ForeignKey("IdUbicacion")]
+    [InverseProperty("Pedidos")]
+    public virtual Ubicacion? IdUbicacionNavigation { get; set; }
+}
