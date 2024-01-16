95021137e78695bf33edc6c7b6a2f8eed9f65d3c
Faa <tj.fabricio.sanchez@gmail.com>
2024-01-15 21:50:44 -0400

    Proyecto Base

---
+++ RestauranteTuliette/RestauranteTuliette/Modelos/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestauranteTuliette.Modelos;
+
+[Table("Usuario")]
+public partial class Usuario
+{
+    [Key]
+    public int IdUsuario { get; set; }
+
+    public int? Nombre { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Contrasena { get; set; }
+
+    public int? Estado { get; set; }
+
+    public int? IdRol { get; set; }
+
+    [ForeignKey("IdRol")]
+    [InverseProperty("Usuarios")]
+    public virtual Rol? IdRolNavigation { get; set; }
+}
