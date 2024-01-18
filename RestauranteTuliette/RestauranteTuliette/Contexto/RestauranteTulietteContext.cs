5aa1f800ca5f797783ee91d5b9b72e6de4b4869e
Faa <tj.fabricio.sanchez@gmail.com>
2024-01-17 22:11:54 -0400

    Bebidum Service

--- RestauranteTuliette/RestauranteTuliette/Contexto/RestauranteTulietteContext.cs
+++ RestauranteTuliette/RestauranteTuliette/Contexto/RestauranteTulietteContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using RestauranteTuliette.Modelos;
 using RestauranteTuliette.Modelos.Entity;
 
 namespace RestauranteTuliette.Contexto;
@@ -52,7 +53,7 @@ public partial class RestauranteTulietteContext : DbContext
         {
             entity.HasKey(e => e.IdPedido).HasName("PK__Pedido__9D335DC38AB35B7E");
 
-            entity.HasOne(d => d.IdBebidaNavigation).WithMany(p => p.PedidosNavigation).HasConstraintName("FK__Pedido__IdBebida__4316F928");
+            entity.HasOne(d => d.IdBebidaNavigation).WithMany(p => p.Pedidos).HasConstraintName("FK__Pedido__IdBebida__4316F928");
 
             entity.HasOne(d => d.IdPlatoNavigation).WithMany(p => p.Pedidos).HasConstraintName("FK__Pedido__IdPlato__4222D4EF");
 
--- RestauranteTuliette/RestauranteTuliette/Controllers/BebidaController.cs
+++ RestauranteTuliette/RestauranteTuliette/Controllers/BebidaController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using RestauranteTuliette.Modelos.DTO;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.RegularExpressions;
-using RestauranteTuliette.Contexto;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Immutable;
+using RestauranteTuliette.Modelos;
 using RestauranteTuliette.Modelos.Entity;
+using RestauranteTuliette.Services.InterfacesService;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using APITiendaBDDic.Modelos.DTO;
 
 namespace RestauranteTuliette.Controllers
 {
@@ -12,75 +13,58 @@ namespace RestauranteTuliette.Controllers
     [ApiController]
     public class BebidaController : ControllerBase
     {
-        private readonly RestauranteTulietteContext _context;
-        public BebidaController(RestauranteTulietteContext context)
+        private readonly IBebidumService _bebidumService;
+
+        public BebidaController(IBebidumService bebidumService)
         {
-            _context = context;
+            _bebidumService = bebidumService;
         }
-        // GET: api/<BebidumController>
+
         [HttpGet]
         public async Task<ActionResult<List<Bebidum>>> Get()
         {
-            return await _context.Bebida.ToListAsync();
+            var bebidas = await _bebidumService.ListaBebidas();
+            return Ok(bebidas);
         }
-        // GET api/<BebidumController>/5
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Bebidum>> Get(int id)
         {
-            return await _context.Bebida.FirstOrDefaultAsync(x => x.IdBebida == id);
+            var bebida = await _bebidumService.ObtenerBebidaPorId(id);
+            if (bebida == null)
+                return NotFound();
+
+            return Ok(bebida);
         }
 
-        // POST api/<BebidumController>
         [HttpPost]
-        public async Task<ActionResult> Post(Bebidum Bebidum)
+        public async Task<ActionResult> Post(Bebidum bebida)
         {
-            if (Bebidum != null)
-            {
-                _context.Bebida.Add(Bebidum);
-                await _context.SaveChangesAsync();
+            var result = await _bebidumService.InsertBebida(bebida);
+            if (result)
                 return Ok();
-            }
-            else { return BadRequest("Debe ingresar datos validos"); }
+            else
+                return BadRequest("Debe ingresar datos válidos");
         }
 
-        // PUT api/<BebidumController>/5
         [HttpPut("{id}")]
-        public async Task<ActionResult> Put(int id, Bebidum Bebidum)
+        public async Task<ActionResult> Put(int id, BebidumDTO bebida)
         {
-            Bebidum Modificar = await _context.Bebida.FirstOrDefaultAsync(x => x.IdBebida == id);
-            if (Modificar != null)
-            {
-                Modificar.TipoBebida = Bebidum.TipoBebida;
-                Modificar.Precio = Bebidum.Precio;
-                Modificar.Descripcion = Bebidum.Descripcion;
-                Modificar.IdIngrediente = Bebidum.IdIngrediente;
-
-                await _context.SaveChangesAsync();
+            var result = await _bebidumService.UpdateBebida(bebida, id);
+            if (result)
                 return Ok();
-            }
             else
-            {
                 return NotFound();
-            }
         }
 
-        // DELETE api/<BebidumController>/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id/*,Bebidum Bebidum*/)
+        public async Task<ActionResult> Delete(int id)
         {
-            Bebidum BebidumEliminar = await _context.Bebida.FirstOrDefaultAsync(x => x.IdBebida == id);
-            if (BebidumEliminar != null)
-            {
-                _context.Remove(BebidumEliminar);
-
-                await _context.SaveChangesAsync();
+            var result = await _bebidumService.DeleteBebida(id);
+            if (result)
                 return Ok();
-            }
             else
-            {
                 return NotFound();
-            }
         }
-        //bebidacontrollerCompleto
     }
 }
