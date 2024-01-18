b4c8da16f9170ce26eec4780e471c0836170d292
horpuma <horpuma@gmail.com>
2024-01-17 21:25:23 -0400

    Carpetas orden y services

--- RestauranteTuliette/RestauranteTuliette/Controllers/IngredientesController.cs
+++ RestauranteTuliette/RestauranteTuliette/Controllers/IngredientesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RestauranteTuliette.Contexto;
-using RestauranteTuliette.Modelos;
+using RestauranteTuliette.Modelos.Entity;
 
 namespace RestauranteTuliette.Controllers
 {
